f00102c338660257c5eef791edac5ef6154b897f
Hanbe.T <hanbet@koba-masa.com>
2020-02-18 23:40:56 +0900

    カウント数を保持する機能を追加。いいね数に応じて時間を変更する機能を追加

--- js/deathst.js
+++ js/deathst.js
@@ -1,4 +1,12 @@
-//$('img#goodMark').click(function() {
+const key = 'good_counter';
+
+$(window).on('load', function() {
+    var nowCounter = readCookie();
+    $('#goodCounter').text(nowCounter);
+    var progresMaxValue = setProgresMaxValue(nowCounter);
+    $('#goodTime').attr('max', progresMaxValue);
+})
+
 function good() {
     var nowCounter = parseInt($('#goodCounter').text(), 10);
     nowCounter = nowCounter + 1;
@@ -15,5 +23,35 @@ function updateTime() {
         setTimeout(updateTime, 100);
     } else {
         $('#goodMark').attr('onclick', '');
+        writeCookie($('#goodCounter').text());
     }
+}
+
+function readCookie() {
+    var cookies = document.cookie.split(';');
+    var goodCounter = 0;
+    for (var i = 0; i < cookies.length; i++) {
+        var cookieArray = cookies[i].split('=');
+        if (cookieArray[0] == key) {
+            goodCounter = cookieArray[1];
+            break;
+        }
+    }
+    return parseInt(goodCounter, 10);
+}
+
+function writeCookie(goodCounter) {
+    document.cookie = key + '=' + encodeURIComponent(goodCounter);
+}
+
+function setProgresMaxValue(nowCounter) {
+    var progresMaxValue = 0;
+    if (nowCounter < 500) {
+        progresMaxValue = 100;
+    } else if (nowCounter < 1000) {
+        progresMaxValue = 150;
+    } else {
+        progresMaxValue = 200;
+    }
+    return progresMaxValue;
 }
